2724beca1afc27271768fd2ce9539abfc87466ae
hora89 <hora2rishi@gmail.com>
2013-07-05 23:46:39 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require ('fs');
+//fs.readFileSync('index.html');
+//console.log(fs);
+
+buf = new Buffer(256);
+buf=fs.readFileSync('index.html');
+//var len = buf.write
+var string = buf.toString('utf8', 0);
+//console.log(string);
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World Bitches!');
+  response.send('Hello World Bitches!'+ string);
 });
 
 var port = process.env.PORT || 5000;
